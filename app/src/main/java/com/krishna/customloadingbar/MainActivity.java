6846ca50e23fc9e0bc98c1ebefda2fbb12df4cd2
Nirav <niravnvk@gmail.com>
2015-06-01 12:19:56 +0530

    Initial commit

---
+++ app/src/main/java/com/krishna/customloadingbar/MainActivity.java
@@ -0,0 +1,25 @@
+package com.krishna.customloadingbar;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.android.customloaders.LoaderType;
+
+
+public class MainActivity extends ActionBarActivity {
+    com.android.customloaders.LoaderView Loader;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Loader = (com.android.customloaders.LoaderView)findViewById(R.id.imageLoader);
+
+        Loader.setLoader(LoaderType.FLIP_FLOP);
+    }
+
+
+
+}
